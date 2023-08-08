019b0bcd25e218886df4507cb88243b958a30910
Akash Mittal <mittal.akash.adam@gmail.com>
2023-08-08 08:46:48 +0200

    Added new files

--- src/main/java/com/am/bp/innovations/App.java
+++ src/main/java/com/am/bp/innovations/App.java
@@ -1,13 +1,25 @@
 package com.am.bp.innovations;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.util.Enumeration;
+
+import org.cryptacular.bean.KeyStoreFactoryBean;
+import org.cryptacular.io.FileResource;
+
+public class App {
+
+    private static final String KS_PATH = "src/test/resources/keystores/";
+
+    public static void main(String[] args) throws Exception {
+        final KeyStoreFactoryBean factory = new KeyStoreFactoryBean();
+        factory.setType("PKCS12");
+        factory.setResource(new FileResource(new File(KS_PATH + "keystore.p12")));
+        factory.setPassword("vtcrypt");
+        Enumeration<String> alises = factory.newInstance().aliases();
+        while (alises.hasMoreElements()) {
+            String element = alises.nextElement();
+            System.out.println(element);
+        }
     }
+
 }
--- src/test/java/com/am/bp/innovations/AppTest.java
+++ src/test/java/com/am/bp/innovations/AppTest.java
@@ -1,10 +1,33 @@
 package com.am.bp.innovations;
 
-import junit.framework.TestCase;
+import static org.testng.Assert.assertEquals;
 
-public class AppTest extends TestCase {
+import java.io.File;
 
-    public void testApp() {
-        assertTrue(true);
+import org.cryptacular.bean.KeyStoreFactoryBean;
+import org.cryptacular.io.FileResource;
+import org.cryptacular.io.Resource;
+import org.junit.gen5.api.Test;
+import org.testng.annotations.DataProvider;
+
+public class AppTest {
+
+    private static final String KS_PATH = "src/test/resources/keystores/";
+
+    @DataProvider(name = "keystore-data")
+    public Object[][] getKeyStoreData() {
+        return new Object[][] { new Object[] { "JCEKS", new FileResource(new File(KS_PATH + "keystore.jceks")), 1, },
+                new Object[] { "JKS", new FileResource(new File(KS_PATH + "keystore.jks")), 1, },
+                new Object[] { "PKCS12", new FileResource(new File(KS_PATH + "keystore.p12")), 1, }, };
     }
+
+    @Test
+    public void testNewInstance(final String type, final Resource resource, final int expectedSize) throws Exception {
+        final KeyStoreFactoryBean factory = new KeyStoreFactoryBean();
+        factory.setType("PKCS12");
+        factory.setResource(new FileResource(new File(KS_PATH + "keystore.jks")));
+        factory.setPassword("vtcrypt");
+        assertEquals(factory.newInstance().size(), expectedSize);
+    }
+
 }
